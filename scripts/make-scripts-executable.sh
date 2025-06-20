36d4fb9d97994330bd5fddc68ddd43f2c10e169b
mklycs <michael.klys@gmail.com>
2025-06-21 00:23:23 +0200

    fixed oopsie

--- scripts/make-scripts-executable.sh
+++ scripts/make-scripts-executable.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-chmod +x ./install-packages.sh
-chmod +x ./configure-wofi.sh
-chmod +x ./configure-powermenu.sh
-chmod +x ./configure-sway.sh
-chmod +x ./adjust-i3status.sh
-chmod +x ./adjust-bash.sh
-chmod +x ./adjust-profile.sh
-chmod +x ./adjust-terminal.sh
-chmod +x ./remove-packages.sh
+chmod +x ./scripts/install-packages.sh
+chmod +x ./scripts/configure-wofi.sh
+chmod +x ./scripts/configure-powermenu.sh
+chmod +x ./scripts/configure-sway.sh
+chmod +x ./scripts/adjust-i3status.sh
+chmod +x ./scripts/adjust-bash.sh
+chmod +x ./scripts/adjust-profile.sh
+chmod +x ./scripts/adjust-terminal.sh
+chmod +x ./scripts/remove-packages.sh
