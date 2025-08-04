a6caec99043fc9d39231a53375cf361068dd9e91
mklycs <michael.klys@gmail.com>
2025-08-04 19:41:07 +0100

    small fix

--- scripts/remove-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-set -e
-
-# remove annoying stuff
-sudo apt purge zathura* foot* imagemagick* -y
-sudo apt remove --purge gnome-keyring libpam-gnome-keyring seahorse -y
-rm ~/.config/autostart/gnome-keyring*.desktop 2>/dev/null || true
-sudo apt autoremove --purge -y
